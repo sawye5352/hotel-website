2b40cd90a3b176ebd348392731f45d898df4a5ca
sawye5352 <5352sawyeyintaung@gmail.com>
2024-01-08 19:44:40 +0700

    try fixing facilities images not appearing

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,12 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        gym: "url('/src/assets/images/facilities/gym.png')",
+        "gym": "url('/src/assets/images/facilities/gym.png')",
         "poolside-bar": "url('src/assets/images/facilities/poolside-bar.png')",
-        spa: "url('src/assets/images/facilities/spa.png')",
-        "swimming-pool":
-          "url('src/assets/images/facilities/swimming-pool.png')",
-        restaurant: "url('src/assets/images/facilities/restaurant.png')",
-        laundary: "url('src/assets/images/facilities/laundary.png')",
+        "spa": "url('src/assets/images/facilities/spa.png')",
+        "swimming-pool": "url('src/assets/images/facilities/swimming-pool.png')",
+        "restaurant": "url('src/assets/images/facilities/restaurant.png')",
+        "laundary": "url('src/assets/images/facilities/laundary.png')",
       },
       colors: {
         primary_blue: "#14274A",
